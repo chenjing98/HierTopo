63e75e0679c9adeb366c959966e1d62c30cde149
chenjing <32995936+chenjing98@users.noreply.github.com>
2020-03-24 21:48:45 +0800

    Add files via upload

---
+++ policynet.py
@@ -0,0 +1,206 @@
+import gym
+import warnings
+from itertools import zip_longest
+
+import numpy as np
+import tensorflow as tf
+from gym.spaces import Discrete
+
+from stable_baselines.a2c.utils import conv, linear, conv_to_fc, batch_to_seq, seq_to_batch, lstm
+from stable_baselines.common.distributions import make_proba_dist_type, CategoricalProbabilityDistribution, \
+    MultiCategoricalProbabilityDistribution, DiagGaussianProbabilityDistribution, BernoulliProbabilityDistribution
+from stable_baselines.common.input import observation_input
+
+from stable_baselines.common.policies import ActorCriticPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+
+class GnnPolicy(ActorCriticPolicy):
+    """
+    Policy object that implements actor critic, using a feed forward neural network.
+
+    :param sess: (TensorFlow session) The current TensorFlow session
+    :param ob_space: (Gym Space) The observation space of the environment
+    :param ac_space: (Gym Space) The action space of the environment
+    :param n_env: (int) The number of environments to run
+    :param n_steps: (int) The number of steps to run for each environment
+    :param n_batch: (int) The number of batch to run (n_envs * n_steps)
+    :param reuse: (bool) If the policy is reusable or not
+    :param layers: ([int]) (deprecated, use net_arch instead) The size of the Neural network for the policy
+        (if None, default to [64, 64])
+    :param net_arch: (list) Specification of the actor-critic policy network architecture (see mlp_extractor
+        documentation for details).
+    :param act_fun: (tf.func) the activation function to use in the neural network.
+    :param feature_extraction: (str) The feature extraction type ("cnn" or "mlp")
+    :param kwargs: (dict) Extra keyword arguments for the nature CNN feature extraction
+    """
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=False, layers=None, net_arch=None,
+                 act_fun=tf.tanh, scale=False, **kwargs):
+        super(GnnPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=reuse,
+                                                scale=scale)
+
+        # hyperparameters for GNN
+        self.input_dim = 1
+        self.hid_dims = [64,64]
+        self.out_dim = 64
+
+        self.num_n = 8 # max_node in the environment
+
+        if layers is not None:
+            warnings.warn("Usage of the `layers` parameter is deprecated! Use net_arch instead "
+                          "(it has a different semantics though).", DeprecationWarning)
+            if net_arch is not None:
+                warnings.warn("The new `net_arch` parameter overrides the deprecated `layers` parameter!",
+                              DeprecationWarning)
+
+        if net_arch is None:
+            if layers is None:
+                layers = [64, 64]
+            net_arch = [dict(vf=layers, pi=layers)]
+
+        with tf.variable_scope("gnn", reuse=reuse):
+            self.gnn_weights, self.gnn_bias = self._para_init()
+            graph_latent = self._egnn(self.processed_obs)
+
+        with tf.variable_scope("model", reuse=reuse):
+
+            pi_latent, vf_latent = mlp_extractor(tf.layers.flatten(graph_latent), net_arch, act_fun)
+
+            self._value_fn = linear(vf_latent, 'vf', 1)
+
+            self._proba_distribution, self._policy, self.q_value = \
+                self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent, init_scale=0.01)
+
+        self._setup_init()
+
+    def step(self, obs, state=None, mask=None, deterministic=False):
+        if deterministic:
+            action, value, neglogp = self.sess.run([self.deterministic_action, self.value_flat, self.neglogp],
+                                                   {self.obs_ph: obs})
+        else:
+            action, value, neglogp = self.sess.run([self.action, self.value_flat, self.neglogp],
+                                                   {self.obs_ph: obs})
+        return action, value, self.initial_state, neglogp
+
+    def proba_step(self, obs, state=None, mask=None):
+        return self.sess.run(self.policy_proba, {self.obs_ph: obs})
+
+    def value(self, obs, state=None, mask=None):
+        return self.sess.run(self.value_flat, {self.obs_ph: obs})
+
+    def _egnn(self, obs):
+        E_adj = obs # adjacent matrix with edge features [N x N]
+
+        X = tf.ones([self.num_n, self.input_dim], tf.float32) # node feature matrix [N, ]
+
+        y = tf.matmul(X, self.gnn_weights[0])
+        y += self.gnn_bias[0]
+        y = tf.matmul(tf.reshape(E_adj,[-1,self.num_n]), y)
+        y = tf.sigmoid(y)
+
+        for l in range(1, len(self.gnn_weights)-1):
+            y = tf.matmul(y, self.gnn_weights[l])
+            y += self.gnn_bias[l]
+            y = tf.reshape(y, [-1,self.num_n,self.hid_dims[l]])
+            y = tf.matmul(E_adj, y)
+            y = tf.sigmoid(y)
+            y = tf.reshape(y,[-1,self.hid_dims[l]])
+        
+        y = tf.matmul(y, self.gnn_weights[-1])
+        y += self.gnn_bias[-1]
+        y = tf.reshape(y, [-1,self.num_n,self.hid_dims[-1]])
+        y = tf.matmul(E_adj, y)
+        y = tf.sigmoid(y)
+
+        return y
+
+    def _para_init(self):
+        """
+        Initializing network parameters.
+        """
+        weights = []
+        bias = []
+
+        curr_in_dim = self.input_dim
+
+        # hidden layers
+        for hid_dim in self.hid_dims:
+          weights.append(glorot([curr_in_dim, hid_dim]))
+          bias.append(zeros([hid_dim]))
+          curr_in_dim = hid_dim
+
+        # output layer
+        weights.append(glorot([curr_in_dim, self.out_dim]))
+        bias.append(zeros([self.out_dim]))
+
+        return weights, bias
+
+def glorot(shape, dtype=tf.float32):
+    # Xavier Glorot & Yoshua Bengio (AISTATS 2010) initialization (Eqn 16)
+    init_range = np.sqrt(6.0 / (shape[0] + shape[1]))
+    init = tf.random_uniform(
+        shape, minval=-init_range, maxval=init_range, dtype=dtype)
+    return tf.Variable(init)
+        
+def zeros(shape, dtype=tf.float32):
+    init = tf.zeros(shape, dtype=dtype)
+    return tf.Variable(init)
+
+def mlp_extractor(flat_observations, net_arch, act_fun):
+    """
+    Constructs an MLP that receives observations as an input and outputs a latent representation for the policy and
+    a value network. The ``net_arch`` parameter allows to specify the amount and size of the hidden layers and how many
+    of them are shared between the policy network and the value network. It is assumed to be a list with the following
+    structure:
+
+    1. An arbitrary length (zero allowed) number of integers each specifying the number of units in a shared layer.
+       If the number of ints is zero, there will be no shared layers.
+    2. An optional dict, to specify the following non-shared layers for the value network and the policy network.
+       It is formatted like ``dict(vf=[<value layer sizes>], pi=[<policy layer sizes>])``.
+       If it is missing any of the keys (pi or vf), no non-shared layers (empty list) is assumed.
+
+    For example to construct a network with one shared layer of size 55 followed by two non-shared layers for the value
+    network of size 255 and a single non-shared layer of size 128 for the policy network, the following layers_spec
+    would be used: ``[55, dict(vf=[255, 255], pi=[128])]``. A simple shared network topology with two layers of size 128
+    would be specified as [128, 128].
+
+    :param flat_observations: (tf.Tensor) The observations to base policy and value function on.
+    :param net_arch: ([int or dict]) The specification of the policy and value networks.
+        See above for details on its formatting.
+    :param act_fun: (tf function) The activation function to use for the networks.
+    :return: (tf.Tensor, tf.Tensor) latent_policy, latent_value of the specified network.
+        If all layers are shared, then ``latent_policy == latent_value``
+    """
+    latent = flat_observations
+    policy_only_layers = []  # Layer sizes of the network that only belongs to the policy network
+    value_only_layers = []  # Layer sizes of the network that only belongs to the value network
+
+    # Iterate through the shared layers and build the shared parts of the network
+    for idx, layer in enumerate(net_arch):
+        if isinstance(layer, int):  # Check that this is a shared layer
+            layer_size = layer
+            latent = act_fun(linear(latent, "shared_fc{}".format(idx), layer_size, init_scale=np.sqrt(2)))
+        else:
+            assert isinstance(layer, dict), "Error: the net_arch list can only contain ints and dicts"
+            if 'pi' in layer:
+                assert isinstance(layer['pi'], list), "Error: net_arch[-1]['pi'] must contain a list of integers."
+                policy_only_layers = layer['pi']
+
+            if 'vf' in layer:
+                assert isinstance(layer['vf'], list), "Error: net_arch[-1]['vf'] must contain a list of integers."
+                value_only_layers = layer['vf']
+            break  # From here on the network splits up in policy and value network
+
+    # Build the non-shared part of the network
+    latent_policy = latent
+    latent_value = latent
+    for idx, (pi_layer_size, vf_layer_size) in enumerate(zip_longest(policy_only_layers, value_only_layers)):
+        if pi_layer_size is not None:
+            assert isinstance(pi_layer_size, int), "Error: net_arch[-1]['pi'] must only contain integers."
+            latent_policy = act_fun(linear(latent_policy, "pi_fc{}".format(idx), pi_layer_size, init_scale=np.sqrt(2)))
+
+        if vf_layer_size is not None:
+            assert isinstance(vf_layer_size, int), "Error: net_arch[-1]['vf'] must only contain integers."
+            latent_value = act_fun(linear(latent_value, "vf_fc{}".format(idx), vf_layer_size, init_scale=np.sqrt(2)))
+
+    return latent_policy, latent_value
---
+++ topoagent.py
@@ -0,0 +1,53 @@
+import os
+import gym
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+from stable_baselines.common.env_checker import check_env
+
+from topoenv import TopoEnv
+from policynet import GnnPolicy
+
+MODEL_NAME = "ppo4topo"
+TENSORBOARD_LOG_DIR =  'tensorlog_topo'
+TRAINSTEPS = 1000000
+
+def env_fn():
+    env = TopoEnv()
+    return env
+
+def main():
+
+    if not os.path.exists(TENSORBOARD_LOG_DIR):
+        os.makedirs(TENSORBOARD_LOG_DIR)
+
+    env_fns = []
+    for _ in range(16):
+        env_fns.append(env_fn)
+
+    vec_env = DummyVecEnv(env_fns)
+
+    model = PPO2(GnnPolicy, vec_env, n_steps=512, verbose=1, tensorboard_log=TENSORBOARD_LOG_DIR)
+
+    print("Model built.")
+    model.learn(total_timesteps=TRAINSTEPS)
+    print("Training terminated.")
+
+    # save model
+    model.save(MODEL_NAME)
+
+    """
+    # Test the model
+    obs = env.reset()
+    stop = False
+    rewards = 0
+    while not stop:
+        action, _states = model.predict(obs)
+        obs, reward, stop = env.step(action)
+        rewards += reward
+    print("Reward: ", rewards)
+    """
+    
+if __name__ == "__main__":
+    main()
