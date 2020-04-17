962684dcf4842c88419996fa05837e43dc5577e1
Zili Meng <zilim@ieee.org>
2020-04-17 03:42:43 -0400

    remove deprecated baselines

--- baseline_new/base_test.py
+++ baseline_new/base_test.py
@@ -3,10 +3,9 @@ import math
 import pickle
 from ego_tree_unit import *
 from permatch import *
-from create_file import *
 from params import args
 
-def compute_reward(state, node_num, demand, degree,degree_penalty):    
+def compute_reward(state, node_num, demand, degree, degree_penalty):    
     D = state.copy()       
 
     # floyd shortest path algorithm
@@ -38,35 +37,23 @@ def compute_reward(state, node_num, demand, degree,degree_penalty):
 
 if __name__ == "__main__":
     node_num = args.node
-    
-    f = create_file(node_num)
-    f.create()
-    
     penalty = args.penalty
-    if args.baseline == 'egotree':
-        for i_ter in range(args.iters):
-            with open(args.folder+str(node_num)+'demand_'+str(i_ter)+'.pk',"rb") as fp:
-                demand = pickle.load(fp)
-            with open(args.folder+str(node_num)+'degree_'+str(i_ter)+'.pk',"rb") as fp:
-                degree = pickle.load(fp)          
-            max_degree = max(degree)
-            test_a = ego_tree_unit(demand,node_num,degree,max_degree)
-            test_a.create_tree()
-            test_a.change_insert()
-            state_a, count = test_a.estab()
-            score_a = compute_reward(state_a, node_num, demand, degree, 50)
-            print(score_a)
-    
-    if args.baseline == 'match':
-        for i_ter in range(args.iters):
-            with open(args.folder+str(node_num)+'demand_'+str(i_ter)+'.pk',"rb") as fp:
-                demand = pickle.load(fp)
-            with open(args.folder+str(node_num)+'degree_'+str(i_ter)+'.pk',"rb") as fp:
-                degree = pickle.load(fp)          
-            max_degree = max(degree)
-            test_b = permatch(demand,node_num,degree)
-            state_b = test_b.matching(args.count)
-            score_b = compute_reward(state_b, node_num, demand, degree, 50)
-            print(score_b)
-        
 
+    for i_ter in range(args.iters):
+        with open(args.folder+str(node_num)+'demand_'+str(i_ter)+'.pk',"rb") as fp:
+            demand = pickle.load(fp)
+        with open(args.folder+str(node_num)+'degree_'+str(i_ter)+'.pk',"rb") as fp:
+            degree = pickle.load(fp)          
+        max_degree = max(degree)
+
+        if args.baseline == 'egotree':
+            test = ego_tree_unit(demand,node_num,degree,max_degree)
+            test.create_tree()
+            test.change_insert()
+            state, count = test.estab()
+        elif args.baseline == 'match':
+            test = permatch(demand,node_num,degree)
+            state = test.matching(args.count)
+                
+        score = compute_reward(state, node_num, demand, degree, 50)
+        print(score)
